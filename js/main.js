c257f13d05a0e60549cec8cf44567c9b60a63e48
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-11-25 15:01:20 +0100

    milestone 1 + file teoria

--- js/main.js
+++ js/main.js
@@ -1,3 +1,47 @@
-//Provo il colloegamento con foglio html
-const title = document.getElementById('title').style.color = "red";
+//ARREY ARMI(10)
+const armi = [
+  "Coltello",
+  "Spada",
+  "Ascia",
+  "Bastone",
+  "Pugnale",
+  "Martello da guerra",
+  "Lancia",
+  "Nunchaku",
+  "Katana",
+  "Machete",
+];
+//ARREY CONCORRENTI(10)
+const concorrenti = [
+  "Marco Rossi",
+  "Giulia Bianchi",
+  "Luca Verdi",
+  "Sara Neri",
+  "Alessandro Gallo",
+  "Francesca Conti",
+  "Matteo Ferri",
+  "Elena Moretti",
+  "Simone Rinaldi",
+  "Chiara Romano",
+];
 
+//MILESTONE 1 - Scelta dell’arma
+
+//1 combattente =  un'arma casuale relativa lista.
+//un'arma po' essere assegnata solo una volta
+
+let assegnazioni = [];
+
+for (let i = 0; i < concorrenti.length; i++) {
+  const armaScelta = armi.splice(Math.floor(Math.random() * armi.length), 1)[0];
+
+  //adesso vado a creare un oggetto che abbia come proprietà nome, arma
+  const concorrenteObj = {
+    nome: concorrenti[i],
+    arma: armaScelta,
+  };
+  // vado ad aggiungere in assegnazioni l'oggetto creato
+  assegnazioni.push(concorrenteObj);
+}
+
+console.log(assegnazioni);
---
+++ theory.md
@@ -0,0 +1,19 @@
+MILESTONE 1 {
+Ragionamento:
+per ogni elemento dell'array concorrenti ad ogni giro di ciclo al concorrente selezionato dovrà essere assegnata un'arma dall'array armi.
+
+uso splice garantisce che ogni arma venga assegnata solo una volta.
+
+splice() 
+-> richiede almeno due argomenti:
+- index che in questo caso va a generare un numero casuale.
+- deleteCount : numero di elementi da rimuovere in questo caso dall'arrey ARMI.
+
+Math.floor()
+-> funzione che mi permette di ottenere un numero intero
+
+Math.random()
+-> funzione che mi permette in questo caso mi serve per generare un numero casuale tra 0 e la lunghezza dell'array ARMI.
+}
+
+
